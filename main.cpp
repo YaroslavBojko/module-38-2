d91f2058b462137a760291e113fcc53115235f95
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-12-12 12:33:23 +0300

    sizePolicy

--- main.cpp
+++ main.cpp
@@ -8,15 +8,18 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    auto mainWindow = new QWidget;
+    auto* mainWindow = new QWidget;
     mainWindow->setMinimumSize(1000, 500);
 
-    auto hbox = new QHBoxLayout(mainWindow);
+    auto* hbox = new QHBoxLayout(mainWindow);
     auto* htmlWindow = new QPlainTextEdit;
     hbox->addWidget(htmlWindow);
 
-    auto webWindow = new QWebEngineView;
+    auto* webWindow = new QWebEngineView;
     hbox->addWidget(webWindow);
+
+    htmlWindow->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    webWindow->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
     mainWindow->show();
     return a.exec();
